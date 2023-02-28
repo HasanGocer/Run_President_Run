ebb365a2487e091c1f32efceeeadc55eb0a87d05
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-02-28 15:41:06 +0300

    v 0.25

--- Assets/Scripts/GateManager.cs
+++ Assets/Scripts/GateManager.cs
@@ -5,4 +5,13 @@ using UnityEngine;
 public class GateManager : MonoSingleton<GateManager>
 {
     public int moneyGatePrice, moneyFieldPrice;
+    [SerializeField] List<GameObject> GatePoses = new List<GameObject>();
+    [SerializeField] List<GameObject> TempGates = new List<GameObject>();
+
+    public void GatePlacement()
+    {
+        foreach (GameObject item in GatePoses)
+            Instantiate(TempGates[Random.Range(0, TempGates.Count)], item.transform.position, item.transform.rotation);
+    }
+
 }
--- Assets/Scripts/TemplateScripts/FinishSystem.cs
+++ Assets/Scripts/TemplateScripts/FinishSystem.cs
@@ -19,14 +19,12 @@ public class FinishSystem : MonoSingleton<FinishSystem>
         GameManager gameManager = GameManager.Instance;
         Buttons buttons = Buttons.Instance;
         MoneySystem moneySystem = MoneySystem.Instance;
-        if (gameManager.level % 10 == 0)
-            StartCoroutine(BarSystem.Instance.BarImageFillAmountIenum());
-        LevelManager.Instance.LevelCheck();
-        buttons.winPanel.SetActive(true);
-        if (gameManager.level % 10 == 0)
-            buttons.barPanel.SetActive(true);
-        buttons.finishGameMoneyText.text = moneySystem.NumberTextRevork(gameManager.addedMoney);
         gameManager.gameStat = GameManager.GameStat.finish;
-        moneySystem.MoneyTextRevork(gameManager.addedMoney);
+        StartCoroutine(BarSystem.Instance.BarImageFillAmountIenum());
+        LevelManager.Instance.LevelCheck();
+        AnimController.Instance.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        buttons.winPanel.SetActive(true);
+        buttons.barPanel.SetActive(true);
+        buttons.finishGameMoneyText.text = moneySystem.NumberTextRevork(gameManager.addedMoney);
     }
 }
